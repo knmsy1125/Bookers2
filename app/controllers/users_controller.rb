e5e4ae214701efb61f474a76a18945b0779f6893
knmsy1125 <knmsy1125@gmail.com>
2021-03-19 01:55:10 +0000

    [Add]app/controllers/users , app/views/users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+  end
+
+  def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if  @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
