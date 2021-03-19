c439e95247d1efc2a068e967c10b295ff10202c3
knmsy1125 <knmsy1125@gmail.com>
2021-03-19 02:40:10 +0000

    [Add]config/routes , rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#index'
+  get "home/about" => "homes#show"
+  post 'books' => 'books#create'
+  resources :books,only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
